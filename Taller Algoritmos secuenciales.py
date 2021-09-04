df08d72c6052bf74af2d99e67e5a3fa3665aa260
Ryuk75 <ricardonava75@outlook.es>
2021-09-04 00:02:12 -0500

    2nd Commit: Primer ejercicio de repaso

--- Taller Algoritmos secuenciales.py
+++ Taller Algoritmos secuenciales.py
@@ -8,4 +8,13 @@ Created on Fri Sep  3 23:42:12 2021
 # Taller de ejercicios secuenciales 
 # Inteligencia Computacional
 
+#Calcule el valor de Y indicando paso a paso como llegó al resultado 
+# 1. y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
 
+print("Calcular y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3" + "\n")
+pasouno = (5+2-5)**2
+pasodos = pasouno * 5+8/2-30
+final = pasodos / 2 * 5 - 3
+print("Primer paso: (5+2-5)**2 = ",pasouno)
+print("Segundo paso: ( (5+2-5)^2 * 5+8/2 -30 ) = ",pasodos)
+print("Resultado final = ", final)
