a2872cc0268d37821fe2e5ba3d27797b573b7acb
Ryuk75 <ricardonava75@outlook.es>
2021-09-04 00:12:59 -0500

    3rd Commit: Segundo ejercicio de repaso

--- Taller Algoritmos secuenciales.py
+++ Taller Algoritmos secuenciales.py
@@ -18,3 +18,20 @@ final = pasodos / 2 * 5 - 3
 print("Primer paso: (5+2-5)**2 = ",pasouno)
 print("Segundo paso: ( (5+2-5)^2 * 5+8/2 -30 ) = ",pasodos)
 print("Resultado final = ", final)
+
+# 2. z=5 n=3 m= z-n
+# y = (( (z+2-n)^2 * m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3
+
+z = 5
+n = 3
+m = z-n
+pasouno = (z+2-n)**2
+pasodos = pasouno * 2 + 8 / 2 - 30
+pasotres = (pasodos / 2 * 5 - 3) **5
+pasocuatro = 15 * 3 - 9/3
+final = pasotres + pasocuatro
+print("Primer paso: (z+2-n)**2 = ",pasouno)
+print("Segundo paso: (( (z+2-n)^2 * m+8/2 -30 ) / 2 * 5 -3)^ 5 = ",pasodos)
+print("Tercer paso: 15 * 3 - 9/3 = ", pasotres)
+print("Resultado final = ", final)
+
